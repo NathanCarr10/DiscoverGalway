aa848988830ab52b97b092bd3ca6b39a5bab921d
NathanCarr10 <nathancarr18@gmail.com>
2025-03-27 14:39:54 +0000

    food-drink page.ts file updated

--- src/app/food-drink/food-drink.page.ts
+++ src/app/food-drink/food-drink.page.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-food-drink',
   templateUrl: './food-drink.page.html',
   styleUrls: ['./food-drink.page.scss'],
+  standalone: true,
+  imports: [CommonModule, FormsModule, IonicModule]
 })
-export class FoodDrinkPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class FoodDrinkPage {
+  foodPlaces = [
+    { name: 'Supermacs', type: 'Fast Food', location: 'Eyre Square' },
+    { name: 'Wooza Pizza', type: 'Italian', location: 'Middle Street' }
+  ];
 }
