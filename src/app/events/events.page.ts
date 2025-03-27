0444c0e6d64bed1beaf3fa30c26e6927f219f172
NathanCarr10 <nathancarr18@gmail.com>
2025-03-27 14:34:17 +0000

    events page updated with events function containing some info that i will update later

--- src/app/events/events.page.ts
+++ src/app/events/events.page.ts
@@ -10,4 +10,17 @@ import { FormsModule } from '@angular/forms';
   standalone: true,
   imports: [CommonModule, FormsModule, IonicModule]
 })
-export class EventsPage {}
+
+
+
+export class EventsPage {
+
+  events = [
+    { name: 'Galway Race Week', date: 'July 27, 2025', location: 'Ballybrit Racecourse' },
+    { name: 'Galway Arts Festival', date: 'July 14, 2025', location: 'Eyre Square' }
+  ];
+  
+
+}
+
+
