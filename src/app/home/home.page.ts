e56155320ba2ee2f55300cf7962360fc597de928
NathanCarr10 <nathancarr18@gmail.com>
2025-03-31 19:07:32 +0100

    weather api added to home page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,20 +1,28 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 import { DataService } from '../services/data.service';
 import { FooterNavComponent } from '../footer-nav/footer-nav.component';
-import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [CommonModule, IonicModule, RouterModule, FooterNavComponent]
+  imports: [CommonModule, IonicModule, RouterModule, FooterNavComponent, HttpClientModule]
 })
 export class HomePage {
-  constructor(private dataService: DataService) {}
+  weather: any;
+
+  constructor(
+    private dataService: DataService,
+    private http: HttpClient
+  ) {
+    this.loadWeather();
+  }
 
   async saveData() {
     await this.dataService.setData('userPreferences', { theme: 'dark' });
@@ -23,5 +31,14 @@ export class HomePage {
   async loadData() {
     const data = await this.dataService.getData('userPreferences');
     console.log('Loaded data:', data);
+  }
+
+  loadWeather() {
+    const apiKey = '9b9a0e0fdb7841c5a1b173245253103';
+    const url = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=Galway`;
+
+    this.http.get(url).subscribe(data => {
+      this.weather = data;
+    });
   }
 }
